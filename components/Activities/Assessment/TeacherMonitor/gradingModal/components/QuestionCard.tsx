538b82759cbc19039d6b2095db93b389081b6bf5
terry.tjandra <terry.tjandra@integrated.ipeka.sch.id>
2026-02-21 14:21:30 +0000

    fix(frontend): correct typo in QuestionCard component

--- components/Activities/Assessment/TeacherMonitor/gradingModal/components/QuestionCard.tsx
+++ components/Activities/Assessment/TeacherMonitor/gradingModal/components/QuestionCard.tsx
@@ -32,7 +32,7 @@ export const QuestionCard: React.FC<QuestionCardProps> = ({
     question,
     participant,
     questionsToRevise,
-    handleToggleQuestionTo revise,
+    handleToggleQuestionToRevise,
     answer,
     grade,
     rawView,
