b471031ad1f505a1920238a2b10b777ee7e27ce6
terry.tjandra <terry.tjandra@integrated.ipeka.sch.id>
2026-02-26 04:43:42 +0000

    feat: make post editor background non-interactive

--- components/PostEditor.tsx
+++ components/PostEditor.tsx
@@ -180,7 +180,7 @@ export const PostEditor: React.FC<PostEditorProps> = ({
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            className="absolute inset-0 bg-black/60 backdrop-blur-sm"
+            className="absolute inset-0 bg-black/60 backdrop-blur-sm pointer-events-none"
         />
         <motion.div
           key="modal"
